30b323406fe6d8f995fe183bff5f25cf060dc194
Antoine <antoine@riviere.tf>
2020-07-09 11:46:27 -0700

    First commit

---
+++ app/src/main/java/org/theta/embedsdkexample/MainActivity.kt
@@ -0,0 +1,30 @@
+package org.theta.embedsdkexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.theta.embedsdk.ui.EmbedFragment
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initThetaEmbedSDKFragment()
+    }
+
+    private fun initThetaEmbedSDKFragment() {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        EmbedFragment.newInstance(
+            partnerId = "YOUR_PARTNER_ID", //mandatory
+            partnerSecret = "YOUR_PARTNER_SECRET", //mandatory
+            currentUserId = "CURRENT_USER_ID", //mandatory
+            currentUsername = "CURRENT_USERNAME" //optionnal
+        ).let { fragment ->
+            transaction.add(R.id.rootContainer, fragment)
+            transaction.commit()
+        }
+    }
+
+}
